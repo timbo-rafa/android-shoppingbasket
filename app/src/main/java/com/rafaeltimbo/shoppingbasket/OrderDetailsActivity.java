8a396bdb1b3db25b5e153582a8f2c3003bb69a6a
Rafael Timbó <timbo.rafa@gmail.com>
2018-05-05 21:03:55 -0400

    orderDetails using productDetails xml

--- app/src/main/java/com/rafaeltimbo/shoppingbasket/OrderDetailsActivity.java
+++ app/src/main/java/com/rafaeltimbo/shoppingbasket/OrderDetailsActivity.java
@@ -2,10 +2,12 @@ package com.rafaeltimbo.shoppingbasket;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -28,20 +30,37 @@ public class OrderDetailsActivity extends AppCompatActivity {
         final Button deliverBtn = findViewById(R.id.deliverBtn);
         final Button cancelOrderBtn = findViewById(R.id.cancelOrderBtn);
 
-        final TextView idtv = findViewById(R.id.orderId);
-        final TextView orderCustomertv = findViewById(R.id.orderCustomer);
-        final TextView datetv = findViewById(R.id.orderDate);
-        final TextView statustv = findViewById(R.id.orderStatus);
-        final TextView pricetv = findViewById(R.id.orderPrice);
-        final TextView producttv = findViewById(R.id.orderProduct);
+        final ImageView imageiv = findViewById(R.id.productDetailsImage);
+        final TextView idtv = findViewById(R.id.productDetailsOrderId);
+        final TextView orderCustomertv = findViewById(R.id.productDetailsCustomer);
+        final TextView datetv = findViewById(R.id.productDetailsOrderDate);
+        final TextView statustv;// = findViewById(R.id.orderStatus);
+        final TextView pricetv = findViewById(R.id.productDetailsPrice);
+        final TextView producttv = findViewById(R.id.productDetailsName);
+        final TextView categorytv = findViewById(R.id.productDetailsCategory);
 
+        final TextView currentQuantitytv = findViewById(R.id.productDetailsCurrentQuantityTag);
+        final TextView quantitytv = findViewById(R.id.productDetailsQuantity);
+        final TextView newQuantityTagtv = findViewById(R.id.productDetailsNewQuantityTag);
+        final TextView inputQuantityTagtv = findViewById(R.id.productDetailsNewQuantity);
+
+        statustv = categorytv;
 
         idtv.setText(order.id);
         orderCustomertv.setText(customer.fullName());
         datetv.setText(order.orderDate);
         statustv.setText(order.status.toString());
+        statustv.setTypeface(null, Typeface.BOLD);
         pricetv.setText(String.format(Locale.CANADA,"$%5.2f", order.price * order.quantity));
-        producttv.setText(product.name);
+        producttv.setText(order.id + " - " + product.name);
+
+        currentQuantitytv.setText(getString(R.string.amountPurchased));
+        quantitytv.setText(order.quantity.toString());
+        newQuantityTagtv.setVisibility(View.GONE);
+        inputQuantityTagtv.setVisibility(View.GONE);
+        //idtv.setVisibility(View.VISIBLE);
+        orderCustomertv.setVisibility(View.VISIBLE);
+        new ImageDownloader(getApplicationContext()).download(imageiv, product.imageUrl);
 
         //default
         cancelOrderBtn.setVisibility(View.VISIBLE);
